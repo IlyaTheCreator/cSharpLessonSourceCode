4bb629eee08e36b86f4f4258e7c1c00f568dd3eb
ilya_the_creator <respondatanytime@gmail.com>
2021-03-12 17:02:39 +0000

    asdf

--- main.cs
+++ main.cs
@@ -1,33 +1,14 @@
-// using System;
-
-// class MainClass {
-//   public static void Main (string[] args) {
-    
-    
-
-//   }
-// }
-
 using System;
 
 class MainClass {
-  public static void Main (string[] arg) {
-    // string userInput = Console.ReadLine();
+  public static void Main (string[] args) {
+    
+    Console.WriteLine("hello");
 
-    // Console.WriteLine(userInput + " - вот это ты сказал");
-    Console.WriteLine(MyClass.add(1, 18));
-
-    poop.Book book1 = new poop.Book("1984", "super author");
-
-    Console.WriteLine(book1.Name + " " + book1.Author);
   }
 }
 
-class MyClass {
-  public static int add(int num1, int num2) {
-    return num1 + num2;
-  }
-}
+
 
 
 
